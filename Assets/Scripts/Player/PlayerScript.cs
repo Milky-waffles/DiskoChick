e3d5fba54a74b4243feb8c0521b1d955f8f836b9
S0u1 <shestaevd@mail.ru>
2020-02-17 22:02:21 +0300

    first step into that shit

---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : PlayerCombo
+{
+    private Rigidbody2D rigidbody;
+    private Vector2 velocity = new Vector2();
+     private Dictionary<int,Inputs> inputMap = new Dictionary<int, Inputs>();
+
+    
+    void Start()
+    { 
+        rigidbody = GetComponent<Rigidbody2D>();
+        mapInit();
+    }
+
+    
+    void Update()
+    {
+        string res = HandleInput();
+        
+        if (res.Equals("Jump")){
+            print("Jump");
+            inputMap.Clear();
+        }
+        
+        if (Input.GetKey(KeyCode.Z)) foreach (var pair in inputMap)
+        {
+            int key = pair.Key;
+            Inputs value = pair.Value;
+            print(key + "/" + value);
+        }
+
+    }
+
+    private string HandleInput(){
+        if (Input.GetKeyUp(KeyCode.D)) return InputResolver(Inputs.RIGHT);
+        if (Input.GetKeyUp(KeyCode.A)) return InputResolver(Inputs.LEFT);
+        if (Input.GetKeyUp(KeyCode.S)) return InputResolver(Inputs.DOWN);
+        if (Input.GetKeyUp(KeyCode.W)) return InputResolver(Inputs.UP);
+        return "none";
+    }
+
+    private string InputResolver(Inputs input){
+        print(inputMap.Count + 1 + " " + input);
+        inputMap.Add(inputMap.Count + 1, input);
+        return comboMatcher(inputMap);
+    }
+}
