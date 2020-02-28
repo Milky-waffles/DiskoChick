1ee5209ba86e10cccb62b2a19404369e48c6096b
S0u1 <shestaevd@mail.ru>
2020-02-28 21:54:16 +0300

    fuckFuckFUCK

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -11,12 +11,33 @@ public class Player : MonoBehaviour
     public new Rigidbody2D rigidbody = null;
     public Animator animator = null;
     public Enums.States currentState = Enums.States.NONE;
+    private Vector2 velocity = new Vector2();
+    private Dictionary<int,Enums.Inputs> inputMap = new Dictionary<int, Enums.Inputs>();
      void Start()
     { 
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         InitStates();
         InitCombos();
+    }
+     void Update()
+    {
+
+       currentState = HandleInput();
+       states.ForEach((x) => {
+           if (x.Name == currentState) {
+               x.StateHandler();
+               inputMap.Clear();
+           }
+           });
+
+       
+        if (Input.GetKey(KeyCode.Z)) foreach (var pair in inputMap)
+        {
+            int key = pair.Key;
+            Enums.Inputs value = pair.Value;
+            print(key + "/" + value);
+        }
     }
     public Enums.States comboMatcher(Dictionary<int,Enums.Inputs> inputList){
      foreach (var combo in combos) if (combo.Compare(inputList)) {print(combo.getName()); return combo.getName();}
@@ -29,6 +50,19 @@ public class Player : MonoBehaviour
     }
     private void InitCombos(){
         combos.Add(new Combo(Enums.States.JUMP, new Dictionary<int, Enums.Inputs>(){{1,Enums.Inputs.DOWN},{2,Enums.Inputs.DOWN},{3,Enums.Inputs.UP},{4,Enums.Inputs.UP}}));
+    }
+
+    private Enums.States HandleInput(){
+        if (Input.GetKeyUp(KeyCode.D)) return InputResolver(Enums.Inputs.RIGHT);
+        if (Input.GetKeyUp(KeyCode.A)) return InputResolver(Enums.Inputs.LEFT);
+        if (Input.GetKeyUp(KeyCode.S)) return InputResolver(Enums.Inputs.DOWN);
+        if (Input.GetKeyUp(KeyCode.W)) return InputResolver(Enums.Inputs.UP);
+        return Enums.States.NONE;
+    }
+
+    private Enums.States InputResolver(Enums.Inputs input){
+        inputMap.Add(inputMap.Count + 1, input);
+        return comboMatcher(inputMap);
     }
 
 }
