14338689a1bea6e70ec9ac62a1127c8007763030
ElenaPichugina <elenapichugina2012@gmail.com>
2020-03-01 20:42:21 +0400

    added menu

---
+++ Assets/Scripts/UIScripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Sprite buttonOnSprite;
+    [SerializeField] private Sprite buttonOffSprite;
+    private Image image;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+
+    public void OnMouseEnter()
+    {
+        image.sprite = buttonOnSprite;
+    }
+
+    public void OnMouseExit()
+    {
+        image.sprite = buttonOffSprite;
+    }
+}
---
+++ Assets/Scripts/UIScripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);  //пока только один рабочий уровень, => 0 - меню, 1 - игровая сцена
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();        //в Юнити игра не закроется, так что можешь не проверять
+    }
+}
