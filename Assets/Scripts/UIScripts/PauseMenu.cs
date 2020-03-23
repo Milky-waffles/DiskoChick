78bfb0a310a06d20fa93017fe2c459925f654f11
ElenaPichugina <elenapichugina2012@gmail.com>
2020-03-23 19:56:03 +0400

    Add UI elements

---
+++ Assets/Scripts/UIScripts/PauseMenu.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [Header("Menu")]
+    private bool gameIsPaused = false;
+    public bool GameIsPaused  { get { return gameIsPaused; } }
+
+    private bool playerIsDead = false;
+    public bool PlayerIsDead
+    {
+        get { return playerIsDead; }
+        set { playerIsDead = value;}
+    }
+
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private GameObject gameOverMenuUI;
+
+    private void Start()
+    {
+        if (pauseMenuUI != null)
+            pauseMenuUI.SetActive(false);
+
+        if (gameOverMenuUI != null)
+            gameOverMenuUI.SetActive(false);
+
+        gameIsPaused = false;
+    }
+
+    private void Update()
+    {
+        if (PlayerIsDead)
+        {
+            GameOver();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape) && !PlayerIsDead)
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    void GameOver()
+    {
+        gameOverMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void LoadMenu()
+    {
+        PlayerIsDead = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void Replay()
+    {
+        PlayerIsDead = false;
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Scripts/UIScripts/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int Score;
+    private TMP_Text text;
+
+    void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+        Score = 0;
+    }
+
+    void Update()
+    {
+        text.text = "Score: " + Score;
+    }
+
+    public void AddPointsToScore(int points)
+    {
+        Score += points;
+    }
+}
